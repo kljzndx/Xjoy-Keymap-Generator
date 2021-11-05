964df2baaa9ca8f4761488ba1bb9d3cfb6f8541f
李继龙 <kljzndx@outlook.com>
2021-11-05 12:44:40 +0800

    完成 JoyconKey 和 XboxKey 枚举编写

--- XjoyKeymapGenerator/Program.cs
+++ XjoyKeymapGenerator/Program.cs
@@ -6,6 +6,54 @@ using System.Threading.Tasks;
 
 namespace XjoyKeymapGenerator
 {
+    enum JoyconKey
+    {
+        L_DPAD_LEFT,
+        L_DPAD_DOWN,
+        L_DPAD_UP,
+        L_DPAD_RIGHT,
+        L_DPAD_SL,
+        L_DPAD_SR,
+        L_SHOULDER,
+        L_TRIGGER,
+        L_CAPTURE,
+        L_MINUS,
+        L_STICK,
+        R_BUT_A,
+        R_BUT_B,
+        R_BUT_X,
+        R_BUT_Y,
+        R_BUT_SL,
+        R_BUT_SR,
+        R_SHOULDER,
+        R_TRIGGER,
+        R_HOME,
+        R_PLUS,
+        R_STICK,
+    }
+
+    enum XboxKey
+    {
+        DISABLE,
+        XUSB_GAMEPAD_DPAD_LEFT,
+        XUSB_GAMEPAD_DPAD_DOWN,
+        XUSB_GAMEPAD_DPAD_UP,
+        XUSB_GAMEPAD_DPAD_RIGHT,
+        XUSB_GAMEPAD_A,
+        XUSB_GAMEPAD_B,
+        XUSB_GAMEPAD_X,
+        XUSB_GAMEPAD_Y,
+        XUSB_GAMEPAD_LEFT_SHOULDER,
+        XUSB_GAMEPAD_RIGHT_SHOULDER,
+        XUSB_GAMEPAD_LEFT_TRIGGER,
+        XUSB_GAMEPAD_RIGHT_TRIGGER,
+        XUSB_GAMEPAD_LEFT_THUMB,
+        XUSB_GAMEPAD_RIGHT_THUMB,
+        XUSB_GAMEPAD_BACK,
+        XUSB_GAMEPAD_START,
+        XUSB_GAMEPAD_GUIDE,
+    }
+
     internal class Program
     {
         static void Main(string[] args)
