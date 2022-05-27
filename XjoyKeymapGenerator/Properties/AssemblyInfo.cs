a5f7b409b2b3d93a70f7720d654d5607d22f85ca
李继龙 <kljzndx@outlook.com>
2022-05-27 18:51:18 +0800

    修改程序集信息

--- XjoyKeymapGenerator/Properties/AssemblyInfo.cs
+++ XjoyKeymapGenerator/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("XjoyKeymapGenerator")]
+[assembly: AssemblyTitle("Xjoy Keymap Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XjoyKeymapGenerator")]
+[assembly: AssemblyCompany("kljzndx")]
+[assembly: AssemblyProduct("Xjoy Keymap Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
